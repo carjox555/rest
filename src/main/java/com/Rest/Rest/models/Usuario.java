9db2e88b4bd74abcb782cb3bf60a8c8a122d4d42
CARJOX <carjox2021@gmail.com>
2022-09-26 23:11:55 -0500

    casaRest

---
+++ src/main/java/com/Rest/Rest/models/Usuario.java
@@ -0,0 +1,35 @@
+package com.Rest.Rest.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Getter
+    @Setter
+    @Entity
+    @Table(name = "usuario")
+    public class Usuario {
+        @Id
+        private String nameUser;
+        @Column
+        private String password;
+        @Column
+        private String namePeople;
+
+        public Usuario(String nameUser, String password, String namePeople) {
+            this.nameUser = nameUser;
+            this.password = password;
+            this.namePeople = namePeople;
+        }
+
+        public Usuario() {
+        }
+    }
+
+
